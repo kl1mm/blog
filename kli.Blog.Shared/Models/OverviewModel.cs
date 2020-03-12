0c106f8225f1dfb6ad039ae54ad9963bf90f9466
Tobi Kli <tobias.klimm@txs.de>
2020-03-12 14:31:07 +0100

    Overview

--- kli.Blog.Shared/Models/OverviewModel.cs
+++ kli.Blog.Shared/Models/OverviewModel.cs
@@ -5,6 +5,7 @@ namespace kli.Blog.Shared.Models
 {
 	public class OverviewModel
 	{
+		public int Id { get; set; } = 0;
 		public string Header { get; set; } = string.Empty;
 		public string Intro { get; set; } = string.Empty;
 		public DateTime Published { get; set; }

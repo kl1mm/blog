92cf5cd2e9e451cf2226c7d977d87ed6a0b5fa91
Tobi Kli <tobias.klimm@txs.de>
2020-03-13 07:55:06 +0100

    Layout refactoring

---
+++ kli.Blog.Client/Pages/Authentication.razor.cs
@@ -0,0 +1,19 @@
+﻿using kli.Blog.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace kli.Blog.Client.Pages
+{
+	public partial class Authentication : ComponentBase
+	{
+		//[Inject] protected HttpClient? Client { get; set; }
+
+		//protected EntryModel? AboutModel { get; set; }
+
+		//protected async override Task OnInitializedAsync()
+		//{
+		//	this.AboutModel = await this.Client.GetJsonAsync<EntryModel>("api/About/me") ?? null;
+		//}
+	}
+}
